bae57cf28e850add363228997b3c116335db9171
Leonel Esguerra <95692348+nhel-ez@users.noreply.github.com>
2023-11-13 06:40:32 +0000

    s

--- scroll.js
+++ scroll.js
@@ -70,48 +70,29 @@ if (!motionMatchMedia.matches) {
 
 //Testimonial
 
-/**
-  Swiper:
-  https://swiperjs.com/
-**/
-const swiper = new Swiper(".swiperCarousel", {
-  slidesPerView: 3,
-  centeredSlides: true,
-  spaceBetween: 10,
-  keyboard: {
-    enabled: true,
-  },
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-});
+var slideIndex = 1;
+showSlides(slideIndex);
 
-const slides = document.getElementsByClassName("swiper-slide");
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    const { className } = slide;
-    if (className.includes("swiper-slide-next")) {
-      swiper.slideNext();
-    } else if (className.includes("swiper-slide-prev")) {
-      swiper.slidePrev();
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
     }
-  });
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
-
-function resizeTextToFit() {
-  const quoteEls = document.getElementsByClassName('quote');
-  for (const el of quoteEls) {
-    el.style.width = el.offsetWidth;
-    el.style.height = el.offsetHeight;
-  }
-  textFit(quoteEls, { maxFontSize: 14 });
-}
-resizeTextToFit();
-addEventListener("resize", (event) => {
-  resizeTextToFit();
-});
